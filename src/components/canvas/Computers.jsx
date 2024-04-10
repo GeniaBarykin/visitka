b5214630badbcd3cc9f31f01d7613fd2704c1e45
GeniaBarykin <467232@student.saxion.nl>
2024-04-10 17:46:47 +0300

    3d mobile size

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,10 +1,10 @@
-import {Suspense} from 'react'
+import {Suspense, useEffect, useState} from 'react'
 import {Canvas} from '@react-three/fiber'
 import {OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ( props ) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
     <mesh>
@@ -21,8 +21,8 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0,-3.25,-1.5]}
+        scale={props.isMobile ? 0.25 : 0.75}
+        position={props.isMobile ? [-5, -1, -1.2] : [0,-3.25,-1.5]}
         rotation={[-0.01,-0.2,-0.1]}
       />
       
@@ -31,6 +31,29 @@ const Computers = () => {
 }
 
 const ComputerCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []); 
+
   return (
     <Canvas    
       shadows
@@ -42,7 +65,7 @@ const ComputerCanvas = () => {
           maxPolarAngle={Math.PI /2}
           minPolarAngle={Math.PI /2}/>
           
-        <Computers />
+        <Computers isMobile={isMobile}/>
         </Suspense>
         <Preload all />
     </Canvas>
